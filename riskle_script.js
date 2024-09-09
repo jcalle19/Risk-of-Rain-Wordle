4adeb81326492f2bab8831cde740c2ce792b19a8
jcalle19 <jcalle19@asu.edu>
2024-09-08 21:40:49 -0700

    Finished common and void items

--- riskle_script.js
+++ riskle_script.js
@@ -1,11 +1,18 @@
 /**
  * TODO: 
- * - Add colors to show users which are correct/partial/incorrect
  * - Make the final guess remain while restricting any future guesses
  * - add health system
- * - add all items
+ * - add all items (greens, reds, lunars, equipment)
+ * - add suggestion and bug page
+ * - add css to make it look passable
  * For next commit:
- * - added try-catch in guess function to make sure item exists in items map
+ * - Finished adding common and void items
+ * If Caught up:
+ * - add new gamemodes
+ * - add score system
+ * - add unique modifiers
+ * - add leaderboards
+ * - add universal daily mode
  */
 
 
@@ -21,9 +28,16 @@ let difficulty = 0;
  * Type: Damage, Utility, Healing
  * Description: (Adding to this as I add more items): 
  * Atk-speed, On-hit, On-kill, On-Proc, AoE, heals, Debuff, Dmg-negation, Chance, Attack, Explosive, Dmg-stat, conditional
- * "when-hurt", consumable, spd-stat, armor, money, items
+ * "when-hurt", consumable, spd-stat, armor, money, items, minions
  * Unlocked: Unlocked, Challenge, DLC
  * Expansion: Vanilla, SotV, SotS
+ * 
+ * Notes to add:
+ * - health-stat includes shield due to the low number of shield items
+ * - when-hurt includes when killed due to the low number of on-player-death items
+ * - conditional includes on-stage-start, against certain enemies, after killing a certain amount, after being in/out of combat, while sprinting
+ * - on a timer, etc. Sorry if its broad but each category only has a couple items and I wanted to keep the number of descriptors to a minimum while 
+ * - still explaining the item
  */
 const items = new Map([
     ["soldiersyringe", /*-----*/["Soldier Syringe", ["Common"], ["Yellow"], ["Damage"], ["Atk-Speed"], ["Unlocked"], ["Vanilla"]]],
@@ -37,7 +51,7 @@ const items = new Map([
     ["blisteringlantern", /*--*/["Blistering Lantern", ["Common"], ["White", "Gold"], ["Damage"], ["Dmg-stat", "when-hurt"], ["Unlocked"], ["SotS"]]],
     ["bundleoffireworks", /*--*/["Bundle of Fireworks", ["Common"], ["Red", "White", "Orange"], ["Damage"], ["Conditional", "Explosive"], ["Challenge"], ["Vanilla"]]],
     ["cautiousslug", /*-------*/["Cautious Slug", ["Common"], ["Blue", "Green"], ["Healing"], ["Conditional", "Heals"], ["Unlocked"], ["Vanilla"]]],
-    ["chronicexpansion", /*---*/["Chronic Expansion", ["Common"], ["Red", "White", "Black"], ["Damage"], ["Conditional", "Dmg-stat", "On-kill"], ["Unlocked"], ["Sots"]]],
+    ["chronicexpansion", /*---*/["Chronic Expansion", ["Common"], ["Red", "White", "Black"], ["Damage"], ["Conditional", "Dmg-stat", "On-kill"], ["Unlocked"], ["SotS"]]],
     ["crowbar", /*------------*/["Crowbar", ["Common"], ["Red", "Grey"], ["Damage"], ["Conditional", "Dmg-stat"], ["Unlocked"], ["Vanilla"]]],
     ["delicatewatch", /*------*/["Delicate Watch", ["Common"], ["Black", "Gold"], ["Damage"], ["Consumable", "Dmg-stat"], ["Unlocked"], ["SotV"]]],
     ["energydrink", /*--------*/["Energy Drink", ["Common"], ["Green", "Blue"], ["Utility"], ["Conditional", "Spd-stat"], ["Unlocked"], ["Vanilla"]]],
@@ -65,7 +79,20 @@ const items = new Map([
     ["57leafclover", /*-------*/["57 Leaf Clover", ["Legendary"], ["Green"], ["Utility"], ["On-Proc"], ["Challenge"], ["Vanilla"]]],
     ["moltenperforator", /*---*/["Molten Perforator", ["Boss/Planet"], ["Orange", "Black"], ["Damage"], ["Chance", "On-hit", "AoE", "Explosive"], ["Unlocked"], ["Vanilla"]]],
     ["shapedglass", /*--------*/["Shaped Glass", ["Lunar"], ["White"], ["Damage"], ["Dmg-stat", "Health-stat"], ["Unlocked"], ["Vanilla"]]],
-    ["polylute", /*-----------*/["Polylute", ["Void"], ["Purple"], ["Damage"], ["Chance", "On-hit", "Attack"], ["Unlocked"], ["SotV"]]],
+    ["polylute", /*-----------*/["Polylute", ["Void"], ["Blue", "Purple"], ["Damage"], ["Chance", "On-hit", "Attack"], ["Unlocked"], ["SotV"]]],
+    ["saferspaces", /*--------*/["Safer Spaces", ["Void"], ["Blue"], ["Utility"], ["Conditional", "Dmg-negation"], ["Unlocked"], ["SotV"]]],
+    ["needletick", /*---------*/["Needletick", ["Void"], ["Purple"], ["Damage"], ["Chance", "On-hit", "Debuff", "Attack"], ["Unlocked"], ["SotV"]]],
+    ["lostseerslenses", /*----*/["Lost Seer's Lenses", ["Void"], ["Blue", "Purple"], ["Damage"], ["Chance", "On-hit", "Attack"], ["Unlocked"], ["SotV"]]],
+    ["weepingfungus", /*------*/["Weeping Fungus", ["Void"], ["Purple"], ["Healing"], ["Conditional", "Heals"], ["Unlocked"], ["SotV"]]],
+    ["encrustedkey", /*-------*/["Encrusted Key", ["Void"], ["White"], ["Utility"], ["Conditional, Items"], ["Unlocked"], ["SotV"]]],
+    ["singularityband", /*----*/["Singularity Band", ["Void"], ["Blue", "Purple"], ["Damage", "Utility"], ["Conditional", "Attack"], ["Unlocked"], ["SotV"]]],
+    ["lysatecell", /*---------*/["Lysate Cell", ["Void"], ["Purple", "Blue"], ["Utility"], ["Survivor-Skill"], ["Unlocked"], ["SotV"]]],
+    ["voidsentflame", /*------*/["Voidsent Flame", ["Void"], ["Orange", "Purple"], ["Damage"], ["Conditional", "On-hit", "Explosive"], ["Unlocked"], ["SotV"]]],
+    ["plasmashrimp", /*-------*/["Plasma Shrimp", ["Void"], ["Blue"], ["Damage"], ["Health-stat", "Conditional", "Attack"], ["Unlocked"], ["SotV"]]],
+    ["tentabauble", /*--------*/["Tentabauble", ["Void"], ["Purple", "Blue"], ["Utility"], ["Chance", "On-hit", "Debuff"], ["Unlocked"], ["SotV"]]],
+    ["benthicbloom", /*-------*/["Benthic Bloom", ["Void"], ["Blue"], ["Utility"], ["Conditional", "Items"], ["Unlocked"], ["SotV"]]],
+    ["pluripotentlarva", /*---*/["Pluripotent Larva", ["Void"], ["Blue", "Black"], ["Utility"], ["Conditional", "when-hurt", "Items"], ["Unlocked"], ["SotV"]]],
+    ["newlyhatchedzoea", /*---*/["Newly Hatched Zoea", ["Void"], ["Purple"], ["Damage"], ["Conditional", "Minions"]]],
 ]);
 
 //Makes array of keys and grabs random index
@@ -156,11 +183,11 @@ function compareAttributes(guessed_attributes, curr_attributes) {
                 color_test_array.push("green");
                 document.getElementById(current_cell_id).style.backgroundColor = "green";
             } else if (correct_count != guessed_attributes[index].length && guessed_attributes[index].length === curr_attributes[index].length) {//same size, not all correct
-                color_test_array.push("orange");
-                document.getElementById(current_cell_id).style.backgroundColor = "orange";
+                color_test_array.push("yellow");
+                document.getElementById(current_cell_id).style.backgroundColor = "yellow";
             } else if (guessed_attributes[index].length != curr_attributes[index].length) { //5, not same size, some correct
-                color_test_array.push("orange");
-                document.getElementById(current_cell_id).style.backgroundColor = "orange";
+                color_test_array.push("yellow");
+                document.getElementById(current_cell_id).style.backgroundColor = "yellow";
             }
         } else { //none correct
             color_test_array.push("red");
