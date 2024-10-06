71b42b3b28300c12de010e44059767cf6d73264a
jcalle19 <jcalle19@asu.edu>
2024-10-06 00:20:23 -0700

    bug fixes and refactoring

--- riskle_script.js
+++ riskle_script.js
@@ -14,16 +14,7 @@
  * - add unique modifiers
  * - add leaderboards
  * - add universal daily mode
- */
-
-
-let player_health;
-let current_item = "none";
-let guessed_item;
-let guess_counter = 0;
-let difficulty = 0;
-
-/*
+ 
  * Notes to add:
  * - health-stat includes shield due to the low number of shield items
  * - when-hurt includes when killed due to the low number of on-player-death items
@@ -31,150 +22,160 @@ let difficulty = 0;
  * - on a timer, etc. Sorry if its broad but each category only has a couple items and I wanted to keep the number of descriptors to a minimum while 
  * - still explaining the item
  */
-const game_state = {
-    current_item : "none",
-}
+    webApp = {
+        status : {
+            current_item : "none",
+            guessed_item : "none",
+            guess_counter : 0,
+            difficulty : 0,
+        },
 
-function start_game() {
-    document.getElementById("gameplay-section").style.display = "block";
-}
-
-//Makes array of keys and grabs random index
-function generateItem() {
-    let itemsArray = Array.from(items.keys());
-    current_item = itemsArray[Math.floor(Math.random() * itemsArray.length)];
-    console.log(current_item);
-}
-
-/** Takes guess from player and then,
- * - Checks for the item in the item map
- * - adds item info in a row
- * - compares with current item
- * - clears the search bars
- */
-function makeGuess(inputText, tableID) {
-    let search_input = document.getElementById("search-input");
-    let guessed_input = search_input.value.toLowerCase().replace(/[\s'-]+/g, ''); //removes special characters and spaces to match keys
-    guessed_item = items.get(guessed_input);
-    //try {
-        addrow(tableID, guessed_item);
-        search_input.value = "";
-        compareItems();
-    //}
-    //catch (error) {
-        //alert("Something went wrong!");
-        //console.log(error);
-    //}
-}
-//Called when user guesses
-function addrow(tableID, new_guess) {
-    let table_proxy = document.getElementById(tableID)
-    let new_row;
-
-    if (guess_counter < 5) {
-        new_row = table_proxy.insertRow(-1);
-        //Inserting cells for all slots
-        for (let cells = 0; cells < 7; cells++) {
-            new_cell = new_row.insertCell(cells);
-            new_cell.id = "row-" + guess_counter + "-cell-" + cells;
-            new_cell.innerHTML = guessed_item[cells].toString().replace(/[,]+/g, '<br>');
-        }
-        guess_counter++;
-    }
-    //update ask counter to reset guesses
-    else deleteGuesses(tableID);
-}
-
-//comparing guessed item and current item
-function compareItems() {
-    let current_attributes = items.get(current_item);
-    let formatted_guess = (guessed_item[0].toLowerCase()).replace(/[\s'-]+/g, ''); //now in key format
-    if (formatted_guess == current_item) {
-        compareAttributes(guessed_item, current_attributes); //debugging, need to remove once finished
-        alert("You Win! :)");
-    }
-    else {
-        compareAttributes(guessed_item, current_attributes);
-    }
-}
-
-/**
- * For each attribute of the guessed and current items
- * 1- check if each item in guessed is in current
- * 2- if yes, check to see if they are the same size
- * 3-- if yes, give the attribute a green color
- * 4-- if no, give the attribute an orange color
- * 5- if no, but some are, then give orange
- * 6- if no, and none are, give red
- */
-function compareAttributes(guessed_attributes, curr_attributes) {
-    let correct_count = 0;
-    let color_test_array = [];
-    let current_cell_id;
-    for (let index = 1; index < guessed_attributes.length; index++) {
-        current_cell_id = `row-${guess_counter - 1}-cell-${index}`;
-        for (let innerIndex = 0; innerIndex < guessed_attributes[index].length; innerIndex++) { //1
-            if (curr_attributes[index].includes(guessed_attributes[index][innerIndex])) {
-                correct_count++;
+        start_game : function() {
+            this.generateItem();
+            document.getElementById("gameplay-section").style.display = "block";
+        },
+    
+        //Makes array of keys and grabs random index
+        generateItem : function() {
+            let itemsArray = Array.from(items.keys());
+            this.status.current_item = itemsArray[Math.floor(Math.random() * itemsArray.length)];
+            console.log(this.status.current_item + " this one");
+        },
+        /** Takes guess from player and then,
+        * - Checks for the item in the item map
+        * - adds item info in a row
+        * - compares with current item
+        * - clears the search bars
+        */
+        makeGuess : function(inputText, tableID) {
+            let search_input = document.getElementById("search-input");
+            let guessed_input = search_input.value.toLowerCase().replace(/[\s'-]+/g, ''); //removes special characters and spaces to match keys
+            this.status.guessed_item = items.get(guessed_input);
+        //try {
+            this.addRow(tableID, this.status.guessed_item);
+            search_input.value = "";
+            this.compareItems();
+        //}
+        //catch (error) {
+            //alert("Something went wrong!");
+            //console.log(error);
+        //}
+        },
+    
+        addRow : function(tableID, new_guess) {
+            let table_proxy = document.getElementById(tableID)
+            let new_row;
+    
+            if (this.status.guess_counter < 5) {
+                new_row = table_proxy.insertRow(-1);
+                //Inserting cells for all slots
+                for (let cells = 0; cells < 7; cells++) {
+                    new_cell = new_row.insertCell(cells);
+                    new_cell.id = "row-" + this.status.guess_counter + "-cell-" + cells;
+                    new_cell.innerHTML = this.status.guessed_item[cells].toString().replace(/[,]+/g, '<br>');
+                }
+                this.status.guess_counter++;
             }
-            console.log(correct_count);
-        }
-        //If all of guessed are in current and they are the same size
-        if (correct_count != 0) {
-            if (correct_count === guessed_attributes[index].length && correct_count === curr_attributes[index].length) { //all correct and same size
-                color_test_array.push("green");
-                color_cells(current_cell_id, "green");
-            } else if (correct_count != guessed_attributes[index].length && guessed_attributes[index].length === curr_attributes[index].length) {//same size, not all correct
-                color_test_array.push("yellow");
-                color_cells(current_cell_id, "yellow");
-            } else if (guessed_attributes[index].length != curr_attributes[index].length) { //5, not same size, some correct
-                color_test_array.push("yellow");
-                color_cells(current_cell_id, "yellow");
+            //update ask counter to reset guesses
+            else this.deleteGuesses(tableID);
+        },
+    
+        //comparing guessed item and current item
+        compareItems : function() {
+            let current_attributes = items.get(this.status.current_item);
+            let formatted_guess = (this.status.guessed_item[0].toLowerCase()).replace(/[\s'-]+/g, ''); //now in key format
+            if (formatted_guess == this.status.current_item) {
+                this.compareAttributes(this.status.guessed_item, current_attributes);
+                alert("You Win! :)");
             }
-        } else { //none correct
-            color_test_array.push("red");
-            color_cells(current_cell_id, "red");
+            else {
+                console.log(this.status.current_item);
+                this.compareAttributes(this.status.guessed_item, current_attributes);
+            }
+        },
+    
+        /**
+        * For each attribute of the guessed and current items
+        * 1- check if each item in guessed is in current
+        * 2- if yes, check to see if they are the same size
+        * 3-- if yes, give the attribute a green color
+        * 4-- if no, give the attribute an orange color
+        * 5- if no, but some are, then give orange
+        * 6- if no, and none are, give red
+        */
+        compareAttributes : function(guessed_attributes, curr_attributes) {
+            let correct_count = 0;
+            let color_test_array = [];
+            let current_cell_id;
+            for (let index = 1; index < guessed_attributes.length; index++) {
+                current_cell_id = `row-${this.status.guess_counter - 1}-cell-${index}`;
+                for (let innerIndex = 0; innerIndex < guessed_attributes[index].length; innerIndex++) { //1
+                    if (curr_attributes[index].includes(guessed_attributes[index][innerIndex])) {
+                        correct_count++;
+                    }
+                    console.log(correct_count);
+                }
+                //If all of guessed are in current and they are the same size
+                if (correct_count != 0) {
+                    if (correct_count === guessed_attributes[index].length && correct_count === curr_attributes[index].length) { //all correct and same size
+                        color_test_array.push("green");
+                        this.color_cells(current_cell_id, "green");
+                    } else if (correct_count != guessed_attributes[index].length && guessed_attributes[index].length === curr_attributes[index].length) {//same size, not all correct
+                        color_test_array.push("yellow");
+                        this.color_cells(current_cell_id, "yellow");
+                    } else if (guessed_attributes[index].length != curr_attributes[index].length) { //5, not same size, some correct
+                        color_test_array.push("yellow");
+                        this.color_cells(current_cell_id, "yellow");
+                    }
+                } else { //none correct
+                    color_test_array.push("red");
+                    this.color_cells(current_cell_id, "red");
+                }
+                correct_count = 0;
+            }
+            console.log(color_test_array);
+        },
+    
+        color_cells : function(cell_id, color) {
+            cellElement = document.getElementById(cell_id);
+            switch (color) {
+                case "green":
+                    cellElement.style.backgroundColor = "#5eae2f";
+                    cellElement.style.borderLeft = "5px solid #408816";
+                    cellElement.style.borderRight = "5px solid #408816";
+                    cellElement.style.borderTop = "5px solid #85da54";
+                    cellElement.style.borderBottom = "5px solid #85da54";
+                    break;
+                case "yellow":
+                    cellElement.style.backgroundColor = "#cdd702";
+                    cellElement.style.borderLeft = "5px solid #a4ab1c";
+                    cellElement.style.borderRight = "5px solid #a4ab1c";
+                    cellElement.style.borderTop = "5px solid #e5ec61";
+                    cellElement.style.borderBottom = "5px solid #e5ec61";
+                    break;
+                case "red":
+                    cellElement.style.backgroundColor = "red";
+                    cellElement.style.borderLeft = "5px solid #9b0202";
+                    cellElement.style.borderRight = "5px solid #9b0202";
+                    cellElement.style.borderTop = "5px solid #f65353";
+                    cellElement.style.borderBottom = "5px solid #f65353";
+                    break;
+                default:
+                    console.log("something went wrong in color_cells function");
+                    break;
+            }
+        },
+    
+        deleteGuesses : function(tableID) {
+            let table_proxy = document.getElementById(tableID);
+            for (let i = 0; i < this.status.guess_counter; i++) {
+                table_proxy.deleteRow(1);
+            }
+            this.status.guess_counter = 0;
+            document.getElementById('gen-item').innerHTML = `The item was: ${this.status.current_item}`;
         }
-        correct_count = 0;
     }
-    console.log(color_test_array);
-}
 
-function color_cells(cell_id, color) {
-    cellElement = document.getElementById(cell_id);
-    switch (color) {
-        case "green":
-            cellElement.style.backgroundColor = "#5eae2f";
-            cellElement.style.borderLeft = "5px solid #408816";
-            cellElement.style.borderRight = "5px solid #408816";
-            cellElement.style.borderTop = "5px solid #85da54";
-            cellElement.style.borderBottom = "5px solid #85da54";
-            break;
-        case "yellow":
-            cellElement.style.backgroundColor = "#cdd702";
-            cellElement.style.borderLeft = "5px solid #a4ab1c";
-            cellElement.style.borderRight = "5px solid #a4ab1c";
-            cellElement.style.borderTop = "5px solid #e5ec61";
-            cellElement.style.borderBottom = "5px solid #e5ec61";
-            break;
-        case "red":
-            cellElement.style.backgroundColor = "red";
-            cellElement.style.borderLeft = "5px solid #9b0202";
-            cellElement.style.borderRight = "5px solid #9b0202";
-            cellElement.style.borderTop = "5px solid #f65353";
-            cellElement.style.borderBottom = "5px solid #f65353";
-            break;
-        default:
-            console.log("something went wrong in color_cells function");
-            break;
-    }
-}
-function deleteGuesses(tableID) {
-    let table_proxy = document.getElementById(tableID);
-    for (let i = 0; i < guess_counter; i++) {
-        table_proxy.deleteRow(1);
-    }
-    guess_counter = 0;
-    document.getElementById('gen-item').innerHTML = `The item was: ${current_item}`;
-}
+
+
 
